0c40a7505e1de27f2d3cf3f1079f44716807607f
TalexCK <talexck@outlook.com>
2023-09-16 18:35:53 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -88,9 +88,6 @@ jsproxy_config({
     'https://www.baidu.com/img/baidu_resultlogo@2.png': {
       replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
     },
-    'https://www.pornhub.com/': {
-      redir: 'https://php.net/'
-    },
     'http://haha.com/': {
       content: 'Hello World'
     },
